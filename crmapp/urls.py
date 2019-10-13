04256b6085976862e6c0c2d89e5fba90825d2037
Gabkings <gabworks51@gmail.com>
2019-10-14 00:59:17 +0300

    fixed the templates statics files loading bug

--- crmapp/urls.py
+++ crmapp/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from marketing.views import HomePage
+from .marketing.views import HomePage
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
