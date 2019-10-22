f2f0221dcc8aa6be665ad5adf878aef66203b746
Gabkings <gabworks51@gmail.com>
2019-10-23 01:02:15 +0300

    added new account URL

--- crmapp/urls.py
+++ crmapp/urls.py
@@ -35,6 +35,9 @@ urlpatterns = [
         'django.contrib.auth.views.logout', {'next_page': '/login/'}
     ),
     # Account related URLs
+    url(r'^account/new/$',
+    'crmapp.accounts.views.account_cru', name='account_new'
+    ),
     url(r'^account/list/$',
         AccountList.as_view(), name='account_list'
     ),
