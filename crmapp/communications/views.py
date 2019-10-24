6c04377eef0b2c23cca80bb43ca12f3adb1f26e0
Gabkings <gabworks51@gmail.com>
2019-10-24 20:18:28 +0300

    created communications form

--- crmapp/communications/views.py
+++ crmapp/communications/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required()
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
 from .models import Communication
 
